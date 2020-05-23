01e2fc94fd97471dc8b638ba0e45082780f84bb2
Cody DallaValle <cody.dallavalle@gmail.com>
2020-05-22 21:08:29 -0400

    PubChemBot now works in Python3

--- PubChemBot6.022.py
+++ PubChemBot6.022.py
@@ -8,30 +8,36 @@ import facebook
 import random
 import time
 import urllib
-import urllib2
+from urllib.request import urlopen
 import re
 import time
 import json
 import sys
 
-authToken = 'YOUR-FB-GRAPH-AUTH-TOKEN-HERE'
-pageId = 'PAGE-ID'
+authToken = 'YOUR_AUTH_TOKEN_HERE'
+pageId = 'YOUR_PAGE_ID'
 
 fb = facebook.GraphAPI(access_token=authToken)
 pcUrl = 'https://pubchem.ncbi.nlm.nih.gov/'
 errorLog = 'errors.txt' # todo: error logging
 
+def rndSleep():
+		timeSleep = random.randint(7*60, 400*60)
+		timeSleepMins = timeSleep / 60
+		print("Sleeping for " + str(timeSleepMins) + " minutes...")
+		time.sleep(timeSleep)
+        
 def getImgUrl(compoundId):
 	return pcUrl + 'rest/pug/compound/cid/' + compoundId + '/PNG'
 
 def getFormula(compoundId):
-	src = urllib2.urlopen(pcUrl + 'rest/pug/compound/cid/' + compoundId + '/property/MolecularFormula,IUPACName/JSON').read()
+	src = urlopen(pcUrl + 'rest/pug/compound/cid/' + compoundId + '/property/MolecularFormula,IUPACName/JSON').read().decode('utf-8')
 	srcJson = json.loads(src)
 	return "Molecular formula: " + srcJson["PropertyTable"]["Properties"][0]["MolecularFormula"] + "\nName: " + srcJson["PropertyTable"]["Properties"][0]["IUPACName"]
 
 def getSynonyms(compoundId):
 	try:
-		src = urllib2.urlopen(pcUrl + 'rest/pug/compound/cid/' + compoundId + '/Synonyms/JSON').read()
+		src = urlopen(pcUrl + 'rest/pug/compound/cid/' + compoundId + '/Synonyms/JSON').read().decode('utf-8')
 		srcJson = json.loads(src)
 		return srcJson["InformationList"]["Information"][0]["Synonym"]
 	except:
@@ -46,23 +52,29 @@ def randomCompound():
 	try:
 		namesList = '\n'.join(names)
 		desc = formula + '\nSynonyms:\n ' + namesList
-		print formula + '\n' + imgUrl + '\n' + namesList
+		print(formula + '\n' + imgUrl + '\n' + namesList)
 		graphPost(imgUrl, desc, cid)
 	except Exception as e:
-		print str(e)
+		print(str(e))
 		
 
 
 def graphPost(imgUrl, desc, cid):
-	resp = fb.put_object(pageId, "photos", url=imgUrl, caption=desc)
-	postId = resp['id']
-	fb.put_object(postId, "comments/comments", message=pcUrl + "compound/" + cid)
+	try:
+		print("Trying to post to Facebook...")
+		resp = fb.put_object(pageId, "photos", url=imgUrl, caption=desc)
+		print("Posted to Facebook, making comments...")
+		postId = resp['id']
+		fb.put_object(postId, "comments/comments", message=pcUrl + "compound/" + cid)
+	except Exception as e:
+		print("Error during Graph post.." + str(e))
 
 while True:
 	try:
 		randomCompound()
-		time.sleep(random.randint(4*60, 18*60))
+		rndSleep()
 	except KeyboardInterrupt:
 		sys.exit()
 	except Exception as e:
-		print "Error: \n" + str(e)
+		print("Error: \n" + str(e))
+		rndSleep()
